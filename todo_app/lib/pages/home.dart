fc0d98df09b24e007b8ce7eabbed2af3540b13f1
shreeya-a <shreeyadhikari11@gmail.com>
2024-04-14 08:59:08 +0545

    user input

--- todo_app/lib/pages/home.dart
+++ todo_app/lib/pages/home.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/pages/checkbox_state.dart';
 
@@ -7,11 +9,10 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: const Todo(),
+      home: const Todo(),
     );
   }
 }
-
 
 class Todo extends StatefulWidget {
   const Todo({super.key});
@@ -21,34 +22,38 @@ class Todo extends StatefulWidget {
 }
 
 class _TodoState extends State<Todo> {
+  final _textController = TextEditingController();
 
-
-  final todo = [
-    CheckBoxState(title: 'xyz'),
-    CheckBoxState(title: 'xyz'),
-    CheckBoxState(title: 'xyz'),
-    CheckBoxState(title: 'xyz'),
-    CheckBoxState(title: 'xyz'),
-  ];
+  String userIPTodo = '';
+  List<CheckBoxState> todo = [];
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text('Todo Manager', style: TextStyle(color: Colors.white),),
-      ),
-      body:Expanded(
-        child:  ListView(
-          // padding: EdgeInsets.all(10),
-          children: [
-            ...todo.map(todoList).toList(),
-
-          ],
+        title: const Text(
+          'Todo Manager',
+          style: TextStyle(color: Colors.white),
         ),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            // display list
+            child: ListView(
+              // padding: EdgeInsets.all(10),
+              children: [
+                ...todo.map(todoList).toList(),
+
+                // ...userIPTodo.map(todoList).toList(),
+                // Text(userIPTodo),
+              ],
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           addTodoDialog();
           // showDialog(context: context, builder: (context){
           //   return Container(
@@ -103,81 +108,112 @@ class _TodoState extends State<Todo> {
           // });
         },
         backgroundColor: Colors.blue,
-
-        child: Icon(Icons.add, color: Colors.white,),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
+    );
+  }
+
+  Widget todoList(CheckBoxState checkbox) => CheckboxListTile(
+        value: checkbox.value,
+        title: Text(
+          checkbox.title,
+          style: TextStyle(
+              decoration: checkbox.value ? TextDecoration.lineThrough : null),
+        ),
+        onChanged: (value) {
+          setState(() {
+            checkbox.value = value!;
+          });
+        },
+        controlAffinity: ListTileControlAffinity.leading,
+        activeColor: Colors.red,
+        checkColor: Colors.green,
+        secondary: const Icon(
+          Icons.delete,
+          color: Colors.red,
+        ),
+      );
+
+  Future addTodoDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) {
+          return Container(
+            child: AlertDialog(
+              shape: RoundedRectangleBorder(),
+              backgroundColor: Colors.white,
+              title: Text('Add a Todo'),
+              content: TextField(
+                controller: _textController,
+                autofocus: true,
+                onChanged: (value) {},
+                decoration: InputDecoration(hintText: 'Type your Todo...'),
+              ),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      final newTodo = CheckBoxState(
+                          title: _textController.text);
+                      todo.add(newTodo);
+                      _textController.clear();
+                    });
+                    Navigator.pop(context);
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.blue),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    'Add',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+
+          );
+          if (userIPTodo != null) {
+            setState(() {
+              todo.add(CheckBoxState(title: userIPTodo));
+            });
+          }
+        }
 
     );
+    if (userIPTodo == null) {
+      setState(() {
+        todo.add(CheckBoxState(title: userIPTodo));
+      });
 
   }
-  Widget todoList(CheckBoxState checkbox) =>  CheckboxListTile(
-    value: checkbox.value,
-    title: Text(checkbox.title, style: TextStyle(
-        decoration: checkbox.value ? TextDecoration.lineThrough : null
-    ),),
-    onChanged: (value){
-      setState(() {
-        checkbox.value = value!;
-      });
-    },
-    controlAffinity: ListTileControlAffinity.leading,
-    activeColor: Colors.red,
-    checkColor: Colors.green,
-    secondary: const Icon(Icons.delete, color: Colors.red,),
-  );
-
-  Future addTodoDialog() => showDialog(context: context, builder: (context){
-    return Container(
-      child: AlertDialog(
-        shape: RoundedRectangleBorder(),
-        backgroundColor: Colors.white,
-        title: Text('Add a Todo'),
-        content: TextField(
-          autofocus: true,
-          onChanged: (value) {},
-          decoration:
-          InputDecoration(hintText: 'Type your Todo...'),
-        ),
-        actions: [
-          ElevatedButton(onPressed: (){
-            Navigator.pop(context);
-          },
-
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-            ),
-            child: Text('Cancel', style: TextStyle(color: Colors.blue),),
-          ),
-
-
-          ElevatedButton(
-            onPressed: (){
-              Navigator.pop(context);
-            },
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-            ),
-
-
-            child: Text('Add',
-              style: TextStyle(color: Colors.white,),
-
-            ),
-          ),
-        ],
-      ) ,
-    );
-  });
-
-
 }
